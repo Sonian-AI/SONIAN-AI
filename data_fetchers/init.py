327992c12ec49d4afa61cf7b532b58f2b3c54d5e
Sonian-AI <cybersonian@gmail.com>
2025-11-11 02:45:52 +0200

    Delete data_fetchers directory

--- data_fetchers/init.py
+++
@@ -1,8 +0,0 @@
-"""
-Data fetchers package init.
-Exports all fetchers.
-"""
-from .cybersonian_fetcher import CybersonianFetcher
-from .solana_dex_fetcher import SolanaDexFetcher
-
-__all__ = ['CybersonianFetcher', 'SolanaDexFetcher']
